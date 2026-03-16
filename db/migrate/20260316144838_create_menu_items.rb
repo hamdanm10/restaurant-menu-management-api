29333433a4404a5f73461cab2d1f29752769b716
hamdanm10 <midunhamdan@gmail.com>
2026-03-16 21:54:13 +0700

    feat: add menu items table

---
+++ db/migrate/20260316144838_create_menu_items.rb
@@ -0,0 +1,17 @@
+class CreateMenuItems < ActiveRecord::Migration[8.1]
+  def change
+    create_table :menu_items do |t|
+      t.string :name, null: false
+      t.text :description, null: true
+      t.decimal :price, precision: 10, scale: 2, null: false
+      t.integer :category, null: true
+      t.boolean :is_available, null: false, default: true
+
+      t.timestamps
+    end
+
+    add_index :menu_items, :name
+    add_index :menu_items, :category
+    add_index :menu_items, [ :category, :is_available ]
+  end
+end
