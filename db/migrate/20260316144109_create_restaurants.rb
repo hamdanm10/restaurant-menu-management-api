25904598c4eded0ad6588f589acfc8bad2bbb8d4
hamdanm10 <midunhamdan@gmail.com>
2026-03-16 21:47:41 +0700

    feat: add restaurants table

---
+++ db/migrate/20260316144109_create_restaurants.rb
@@ -0,0 +1,14 @@
+class CreateRestaurants < ActiveRecord::Migration[8.1]
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.string :address, null: false
+      t.string :phone, null: true
+      t.string :opening_hours, null: false
+
+      t.timestamps
+    end
+
+    add_index :restaurants, :name
+  end
+end
